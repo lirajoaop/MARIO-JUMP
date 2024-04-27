cbda7e4d3528ce3f9f634bfbb9cccef0c9fb12f4
lirajoaop <joaopedroababa132@gmail.com>
2024-04-27 14:19:13 -0300

    animação de morte

--- js/script.js
+++ js/script.js
@@ -17,8 +17,16 @@ const loop = setInterval(() => {
     const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '')
 
     if (pipePosition <= 120 && pipePosition > 0 && marioPosition < 80) {
+
         pipe.style.animation = 'none'
         pipe.style.left = `${pipePosition}px`
+
+        mario.style.animation = 'none'
+        mario.style.bottom = `${marioPosition}px`
+
+        mario.src = './images/game-over.png'
+        mario.style.width = '75px'
+        mario.style.marginLeft = '50px'
     }
 
 }, 10)
